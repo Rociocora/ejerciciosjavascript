2de2bb2857937d1a2ef4cc05239eb3f3725e53dd
Rociocora <161638863+Rociocora@users.noreply.github.com>
2024-05-01 21:13:20 +0200

    Ejercicios_para_practicar

--- js/e2-guardarinformacion.js
+++ js/e2-guardarinformacion.js
@@ -10,16 +10,15 @@ let miNombre;
 //2- INICIAR UNA VARIABLE
 //Primero declarar una variable y luego iniciarla
 document.write("Ejemplo 1: INICIAR UNA VARIABLE CIUDAD </br>");
-let ciudad;
-ciudad = "Malaga";
-document.write("La variable ciudad tiene el valor: " + ciudad);
-
+let lugar;
+lugar = "Malaga";
+document.write("La variable ciudad tiene el valor: " + lugar);
 //Podemos declarar una variable e iniciarla a la vez.
 document.write("</br> EJEMPLO 2: INICIAR UNA VARIABLE EDAD </br>");
-let edad = 33;
+let age = 33;
 //cambiar el valor de la variable edad
-edad = 45;
-document.write("La variable edad tiene el valor: " + edad);
+age = 45;
+document.write("La variable edad tiene el valor: " + age);
 //3- TIPOS DE VARIABLES
 //NUMEROS
 document.write("</br> EJEMPLO 3: CREAR UNA VARRIABLE DE TIPO NUMERO </br>");
@@ -40,4 +39,114 @@ document.write("La variable Esta Soleado es: " + estaSoleado);
 document.write("</br>");
 let esDeDia = false;
 document.write("La variable es de día es: " + esDeDia);
+document.write("</br>");
+//DATOS PERSONALES 
 
+document.write("</br> DATOS PERSONALES </br>");
+let nombre = "Rocio";
+document.write("Nombre: " + nombre);
+document.write("</br>");
+let apellidos = "Cordoba Ramos";
+document.write("Apellidos: " + apellidos);
+document.write("</br>");
+let edad = 31;
+document.write("Edad: " + edad);
+document.write("</br>");
+let altura = 1.56;
+document.write("Altura: " + altura);
+document.write("</br>");
+let ciudad = "Malaga";
+document.write("Ciudad: " + ciudad);
+document.write("</br>");
+let meGustaLaPlaya = true;
+document.write("Me gusta la playa: " + meGustaLaPlaya);
+document.write("</br>");
+let meGustaLaMontaña = false;
+document.write("Me gusta la montaña: " + meGustaLaMontaña);
+document.write("</br>");
+let meGustaElChocolate = true;
+document.write("Me gusta el chocolate: " + meGustaElChocolate);
+document.write("</br>");
+let tengoMascosta = true;
+document.write("Tengo Mascota: " + tengoMascosta);
+document.write("</br>");
+
+document.write("</br> PELICULA FAVORITA </br>");
+let titulo = "Un paseo para recordar";
+document.write("Titulo: " + titulo);
+document.write("</br>");
+let director = "Adam Shankman";
+document.write("Director: " + director);
+document.write("</br>");
+let añoLanzamiento = 2002;
+document.write("Año de Lanzamiento: " + añoLanzamiento);
+document.write("</br>");
+let genero = "Drama";
+document.write("Genero: " + genero);
+document.write("</br>");
+let duracion = "100 min";
+document.write("Duración: " + duracion);
+document.write("</br>");
+let tienePremios = true;
+document.write("¿Tiene premios?: " + tienePremios);
+document.write("</br>");
+
+document.write("</br> CANCIÓN FAVORITA </br>");
+let tituloCancion = "Son de Amores";
+document.write("Titulo: " + tituloCancion);
+document.write("</br>");
+let artista = "Andy y Lucas";
+document.write("Artista: " + artista);
+document.write("</br>");
+let album = "Andy y Lucas";
+document.write("Álbum: " + album);
+document.write("</br>");
+let añoLanzamientoCancion = 2003;
+document.write("Año de Lanzamiento: " + añoLanzamientoCancion);
+document.write("</br>");
+let duracionCancion = "3.51 min";
+document.write("Duración: " + duracionCancion);
+document.write("</br>");
+let tieneVideoclip = true;
+document.write("¿Tiene Videoclip?: " + tieneVideoclip);
+document.write("</br>");
+
+document.write("</br> LIBRO FAVORITO </br>");
+let nombreLibro = "En los zapatos de Valeria";
+document.write("Titulo del Libro: " + nombreLibro);
+document.write("</br>");
+let autor = "Elisabet Benavent";
+document.write("Autor: " + autor);
+document.write("</br>");
+let generoLiterario = "Romance";
+document.write("Genero Literario: " + generoLiterario);
+document.write("</br>");
+let añoPublicacion = 2013;
+document.write("Año de Publicación: " + añoPublicacion);
+document.write("</br>");
+let numeroPaginas = "400 páginas";
+document.write("Número de Páginas: " + numeroPaginas);
+document.write("</br>");
+let tienePelicula = true;
+document.write("¿Tiene Película?: " + tienePelicula);
+document.write("</br>");
+
+document.write("</br> DEPORTE FAVORITO </br>");
+let deporte = "Crossfit";
+document.write("Deporte: " + deporte);
+document.write("</br>");
+let lugarOrigen = "California";
+document.write("Lugar de origen: " + lugarOrigen);
+document.write("</br>");
+let popularidad = 8;
+document.write("Popularidad: " + popularidad);
+document.write("</br>");
+let equipamientoNecesario = "Comba, Cajón, Cuerdas, Mancuernas, Barras y discos, Pesas rusas, Balón medicinal... ";
+document.write("Equipamiento Necesario: " + equipamientoNecesario);
+document.write("</br>");
+let deporteEquipo = false;
+document.write("¿Es un deporte de equipo?: " + deporteEquipo);
+document.write("</br>");
+let practicaExteriores = true;
+document.write("¿Se practica en exteriores?: " + practicaExteriores);
+document.write("</br>");
