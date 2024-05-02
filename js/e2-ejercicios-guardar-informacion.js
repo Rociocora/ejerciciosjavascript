c1d59e62a68449a07f944ac7c56cb8701fdde7cd
Rociocora <161638863+Rociocora@users.noreply.github.com>
2024-05-02 14:01:59 +0200

    ejemplos-constantes-guardarinformacion

---
+++ js/e2-ejercicios-guardar-informacion.js
@@ -0,0 +1,140 @@
+//DATOS PERSONALES (ejercicio 1)
+
+document.write("DATOS PERSONALES </br>");
+let nombre = "Rocio";
+nombre= "Mario";
+document.write("Nombre: " + nombre);
+document.write("</br>");
+let apellidos = "Cordoba Ramos";
+apellidos= "Valencia Muñoz";
+document.write("Apellidos: " + apellidos);
+document.write("</br>");
+let edad = 31;
+edad = 35;
+document.write("Edad: " + edad);
+document.write("</br>");
+let altura = 1.56;
+altura = 1.79;
+document.write("Altura: " + altura);
+document.write("</br>");
+let ciudad = "Malaga";
+ciudad = "Villanueva del Trabuco"
+document.write("Ciudad: " + ciudad);
+document.write("</br>");
+let meGustaLaPlaya = true;
+document.write("Me gusta la playa: " + meGustaLaPlaya);
+document.write("</br>");
+let meGustaLaMontaña = false;
+document.write("Me gusta la montaña: " + meGustaLaMontaña);
+document.write("</br>");
+let meGustaElChocolate = true;
+document.write("Me gusta el chocolate: " + meGustaElChocolate);
+document.write("</br>");
+let tengoMascosta = true;
+document.write("Tengo Mascota: " + tengoMascosta);
+document.write("</br>");
+
+// PELICULA FAVORITA (ejercicio 2)
+document.write("</br> PELICULA FAVORITA </br>");
+let titulo = "Un paseo para recordar";
+document.write("Titulo: " + titulo);
+document.write("</br>");
+let director = "Adam Shankman";
+document.write("Director: " + director);
+document.write("</br>");
+let añoLanzamiento = 2002;
+document.write("Año de Lanzamiento: " + añoLanzamiento);
+document.write("</br>");
+let genero = "Drama";
+document.write("Genero: " + genero);
+document.write("</br>");
+let duracion = "100 min";
+document.write("Duración: " + duracion);
+document.write("</br>");
+let tienePremios = true;
+document.write("¿Tiene premios?: " + tienePremios);
+document.write("</br>");
+
+//Pelicula favorita cambiando algunos datos
+document.write("</br> PELICULA FAVORITA (cambiar algunos datos) </br>");
+titulo = "Titanic";
+document.write("Titulo: " + titulo);
+document.write("</br>");
+director = "Adam Shankman";
+document.write("Director: " + director);
+document.write("</br>");
+añoLanzamiento = 2004;
+document.write("Año de Lanzamiento: " + añoLanzamiento);
+document.write("</br>");
+genero = "animado";
+document.write("Genero: " + genero);
+document.write("</br>");
+duracion = "140 min";
+document.write("Duración: " + duracion);
+document.write("</br>");
+tienePremios = true;
+document.write("¿Tiene premios?: " + tienePremios);
+document.write("</br>");
+
+//CANCIÓN FAVORITA (ejercicio 3)
+document.write("</br> CANCIÓN FAVORITA </br>");
+let tituloCancion = "Son de Amores";
+document.write("Titulo: " + tituloCancion);
+document.write("</br>");
+let artista = "Andy y Lucas";
+document.write("Artista: " + artista);
+document.write("</br>");
+let album = "Andy y Lucas";
+document.write("Álbum: " + album);
+document.write("</br>");
+let añoLanzamientoCancion = 2003;
+document.write("Año de Lanzamiento: " + añoLanzamientoCancion);
+document.write("</br>");
+let duracionCancion = "3.51 min";
+document.write("Duración: " + duracionCancion);
+document.write("</br>");
+let tieneVideoclip = true;
+document.write("¿Tiene Videoclip?: " + tieneVideoclip);
+document.write("</br>");
+
+//LIBRO FAVORITO (Ejercicio 4)
+document.write("</br> LIBRO FAVORITO </br>");
+let nombreLibro = "En los zapatos de Valeria";
+document.write("Titulo del Libro: " + nombreLibro);
+document.write("</br>");
+let autor = "Elisabet Benavent";
+document.write("Autor: " + autor);
+document.write("</br>");
+let generoLiterario = "Romance";
+document.write("Genero Literario: " + generoLiterario);
+document.write("</br>");
+let añoPublicacion = 2013;
+document.write("Año de Publicación: " + añoPublicacion);
+document.write("</br>");
+let numeroPaginas = "400 páginas";
+document.write("Número de Páginas: " + numeroPaginas);
+document.write("</br>");
+let tienePelicula = true;
+document.write("¿Tiene Película?: " + tienePelicula);
+document.write("</br>");
+
+//DEPORTE FAVORITO (ejercicio 5)
+document.write("</br> DEPORTE FAVORITO </br>");
+let deporte = "Crossfit";
+document.write("Deporte: " + deporte);
+document.write("</br>");
+let lugarOrigen = "California";
+document.write("Lugar de origen: " + lugarOrigen);
+document.write("</br>");
+let popularidad = 8;
+document.write("Popularidad: " + popularidad);
+document.write("</br>");
+let equipamientoNecesario = "Comba, Cajón, Cuerdas, Mancuernas, Barras y discos, Pesas rusas, Balón medicinal... ";
+document.write("Equipamiento Necesario: " + equipamientoNecesario);
+document.write("</br>");
+let deporteEquipo = false;
+document.write("¿Es un deporte de equipo?: " + deporteEquipo);
+document.write("</br>");
+let practicaExteriores = true;
+document.write("¿Se practica en exteriores?: " + practicaExteriores);
+document.write("</br>");
--- js/e2-guardarinformacion.js
+++ js/e2-guardarinformacion.js
@@ -19,6 +19,7 @@ let age = 33;
 //cambiar el valor de la variable edad
 age = 45;
 document.write("La variable edad tiene el valor: " + age);
+
 //3- TIPOS DE VARIABLES
 //NUMEROS
 document.write("</br> EJEMPLO 3: CREAR UNA VARRIABLE DE TIPO NUMERO </br>");
@@ -26,12 +27,14 @@ let numero = 5;
 let numeroDecimales = 5.5;
 document.write("El numero sin decimales es: " + numero);
 document.write("</br>El numero sin decimales es: " + numeroDecimales);
+
 //CADENAS (se pueden usar comillas dobles """ o comillas simples '' pero sin mezclarlas) Cuidad con caracteres especiales como comas dentro del texto.
 document.write("</br> EJEMPLO 4: CREAR UNA VARRIABLE DE TIPO TEXTO </br>");
 let saludo = "Hola que tal";
 document.write("La variable de tipo saludo: " + saludo);
 let despedida ='Adiós, me voy';
 document.write("</br>" + despedida);
+
 //BOOLEANOS, true o false. En minusucla y sin comillas.
 document.write("</br> EJEMPLO 5: CREAR UNA VARRIABLE DE TIPO BOOLENO </br>");
 let estaSoleado = true;
@@ -40,138 +43,14 @@ document.write("</br>");
 let esDeDia = false;
 document.write("La variable es de día es: " + esDeDia);
 document.write("</br>");
-//DATOS PERSONALES 
 
-document.write("</br> DATOS PERSONALES </br>");
-let nombre = "Rocio";
-nombre= "Mario";
-document.write("Nombre: " + nombre);
-document.write("</br>");
-let apellidos = "Cordoba Ramos";
-apellidos= "Valencia Muñoz";
-document.write("Apellidos: " + apellidos);
-document.write("</br>");
-let edad = 31;
-edad = 35;
-document.write("Edad: " + edad);
-document.write("</br>");
-let altura = 1.56;
-altura = 1.79;
-document.write("Altura: " + altura);
-document.write("</br>");
-let ciudad = "Malaga";
-ciudad = "Villanueva del Trabuco"
-document.write("Ciudad: " + ciudad);
-document.write("</br>");
-let meGustaLaPlaya = true;
-document.write("Me gusta la playa: " + meGustaLaPlaya);
-document.write("</br>");
-let meGustaLaMontaña = false;
-document.write("Me gusta la montaña: " + meGustaLaMontaña);
-document.write("</br>");
-let meGustaElChocolate = true;
-document.write("Me gusta el chocolate: " + meGustaElChocolate);
-document.write("</br>");
-let tengoMascosta = true;
-document.write("Tengo Mascota: " + tengoMascosta);
+//CONSTANTES
+//SE USAN PARA GUARDAR VALORES QUE NUNCA CAMBIAN, POR EJEMPLO LOS DIAS DE LA SEMANA. ACONSEJABLE PONER EN MAYUSCULA
+document.write("</br> EJEMPLO 6: CREAR UNA CONSTANTE </br>");
+const NUMERODIASSEMANA = 7;
+document.write("La semana tiene " + NUMERODIASSEMANA + " días");
 document.write("</br>");
 
-document.write("</br> PELICULA FAVORITA </br>");
-let titulo = "Un paseo para recordar";
-document.write("Titulo: " + titulo);
-document.write("</br>");
-let director = "Adam Shankman";
-document.write("Director: " + director);
-document.write("</br>");
-let añoLanzamiento = 2002;
-document.write("Año de Lanzamiento: " + añoLanzamiento);
-document.write("</br>");
-let genero = "Drama";
-document.write("Genero: " + genero);
-document.write("</br>");
-let duracion = "100 min";
-document.write("Duración: " + duracion);
-document.write("</br>");
-let tienePremios = true;
-document.write("¿Tiene premios?: " + tienePremios);
-document.write("</br>");
-
-document.write("</br> PELICULA FAVORITA (cambiar algunos datos) </br>");
-titulo = "Titanic";
-document.write("Titulo: " + titulo);
-document.write("</br>");
-director = "Adam Shankman";
-document.write("Director: " + director);
-document.write("</br>");
-añoLanzamiento = 2004;
-document.write("Año de Lanzamiento: " + añoLanzamiento);
-document.write("</br>");
-genero = "animado";
-document.write("Genero: " + genero);
-document.write("</br>");
-duracion = "140 min";
-document.write("Duración: " + duracion);
-document.write("</br>");
-tienePremios = true;
-document.write("¿Tiene premios?: " + tienePremios);
-document.write("</br>");
-
-document.write("</br> CANCIÓN FAVORITA </br>");
-let tituloCancion = "Son de Amores";
-document.write("Titulo: " + tituloCancion);
-document.write("</br>");
-let artista = "Andy y Lucas";
-document.write("Artista: " + artista);
-document.write("</br>");
-let album = "Andy y Lucas";
-document.write("Álbum: " + album);
-document.write("</br>");
-let añoLanzamientoCancion = 2003;
-document.write("Año de Lanzamiento: " + añoLanzamientoCancion);
-document.write("</br>");
-let duracionCancion = "3.51 min";
-document.write("Duración: " + duracionCancion);
-document.write("</br>");
-let tieneVideoclip = true;
-document.write("¿Tiene Videoclip?: " + tieneVideoclip);
-document.write("</br>");
-
-document.write("</br> LIBRO FAVORITO </br>");
-let nombreLibro = "En los zapatos de Valeria";
-document.write("Titulo del Libro: " + nombreLibro);
-document.write("</br>");
-let autor = "Elisabet Benavent";
-document.write("Autor: " + autor);
-document.write("</br>");
-let generoLiterario = "Romance";
-document.write("Genero Literario: " + generoLiterario);
-document.write("</br>");
-let añoPublicacion = 2013;
-document.write("Año de Publicación: " + añoPublicacion);
-document.write("</br>");
-let numeroPaginas = "400 páginas";
-document.write("Número de Páginas: " + numeroPaginas);
-document.write("</br>");
-let tienePelicula = true;
-document.write("¿Tiene Película?: " + tienePelicula);
-document.write("</br>");
-
-document.write("</br> DEPORTE FAVORITO </br>");
-let deporte = "Crossfit";
-document.write("Deporte: " + deporte);
-document.write("</br>");
-let lugarOrigen = "California";
-document.write("Lugar de origen: " + lugarOrigen);
-document.write("</br>");
-let popularidad = 8;
-document.write("Popularidad: " + popularidad);
-document.write("</br>");
-let equipamientoNecesario = "Comba, Cajón, Cuerdas, Mancuernas, Barras y discos, Pesas rusas, Balón medicinal... ";
-document.write("Equipamiento Necesario: " + equipamientoNecesario);
-document.write("</br>");
-let deporteEquipo = false;
-document.write("¿Es un deporte de equipo?: " + deporteEquipo);
-document.write("</br>");
-let practicaExteriores = true;
-document.write("¿Se practica en exteriores?: " + practicaExteriores);
+const NUMEROPI = 3.14;
+document.write("La constante número Pi es " + NUMEROPI);
 document.write("</br>");
